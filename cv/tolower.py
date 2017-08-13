dad9e490b449dd616827fa164399aa2da424dc61
mitchmcdee <mitchmcdee@gmail.com>
2017-08-13 12:31:29 +1000

    Removed unnecessary files

--- cv/tolower.py
+++
@@ -1,8 +0,0 @@
-with open('dictionary.txt', 'r') as fileinput:
-    with open('words3.txt',"w") as fileout:
-        for line in fileinput:
-            fileout.write(line.lower())
-    
-
-
-       
