8d55a31ce629a6e64d138b8873b52bd450bd0219
mitchmcdee <mitchmcdee@gmail.com>
2017-08-13 12:30:06 +1000

    Locked down gen trie to just work with words.txt

--- genTrie.py
+++ genTrie.py
@@ -3,13 +3,16 @@ import sys
 import os
 from Trie import Trie, TrieNode
 
-# Generates a trie when given a file and optionally the maximum amount of words to add
-def genTrie(fileName, maxWords):
+# Log reason for quitting and quit
+def quit(reason):
+    logger.critical(reason)
+    sys.exit(0)
+
+# Generates a trie and optionally limits the amount of words to add
+def genTrie(maxWords):
     trie = Trie()
-
     numWords = 0
-    with open(fileName, "r") as f:
-
+    with open('words.txt', "r") as f:
         # Loop over words an add them to the Trie
         for i, word in enumerate(f):
             numWords += trie.addWord(word.strip('\n'))
@@ -17,24 +20,15 @@ def genTrie(fileName, maxWords):
                 break
 
     print("Trie generated! Added " + str(numWords) + " words.")
-    print("Dumping to .pickle file")
+    print("Dumping to words.pickle file")
     # Save to .pickle file
-    with open(fileName.split('.')[0] + '.pickle', 'wb') as f:
+    with open('words.pickle', 'wb') as f:
         pickle.dump(trie, f, pickle.HIGHEST_PROTOCOL)
         print("Dump complete (lol)")
 
-# Check file
-if len(sys.argv) == 1 or not os.path.exists(sys.argv[1]):
-    print('Must provide a valid word list file')
-    sys.exit(0)
-
 # Check word limit
-if len(sys.argv) > 2 and not sys.argv[2].isdigit():
-    print('Must provide a valid word limit')
-    sys.exit(0)
-
-fileName = sys.argv[1]
-maxWords = int(sys.argv[2]) if len(sys.argv) == 3 else sys.maxsize
+if len(sys.argv) > 1 and not sys.argv[1].isdigit():
+    quit('Must provide a valid word limit')
 
 # Generate Trie
-genTrie(fileName, maxWords)
+genTrie(int(sys.argv[1]) if len(sys.argv) == 2 else sys.maxsize)
